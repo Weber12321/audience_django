5646b33125dacf374909263ae1576453c34dbec4
weifan <weifan@macos.me>
2021-03-16 15:37:03 +0800

    調整管理介面

--- labeling_jobs/admin.py
+++ labeling_jobs/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
 # Register your models here.
+from labeling_jobs.models import Job, Label, Document
+
+
+class LabelInline(admin.TabularInline):
+    model = Label
+    extra = 0
+    fields = ['name', 'description']
+
+
+class DocumentInline(admin.StackedInline):
+    model = Document
+    extra = 0
+    fields = ['title', 's_area_id', 'author', 'content', 'post_time']
+
+
+class JobAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None, {
+                "fields": ['name', 'description']
+            }),
+    ]
+    inlines = [LabelInline, DocumentInline]
+    list_display = ('name', 'show_labels', 'show_document_count', 'created_at', 'updated_at')
+    list_filter = ['created_at', "updated_at"]
+    search_fields = ['name', 'description']
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    model = Document
+    list_display = ['title', 's_area_id', 'author', 'post_time', 'job', 'show_labels']
+    search_fields = ['title', 's_area_id']
+    list_filter = ['post_time']
+
+
+class LabelAdmin(admin.ModelAdmin):
+    model = Label
+    list_display = ('name', 'job', 'show_document_count', 'created_at', 'updated_at')
+    list_filter = ['created_at', "updated_at"]
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Label, LabelAdmin)
--- labeling_jobs/models.py
+++ labeling_jobs/models.py
@@ -6,29 +6,59 @@ from django.db import models
 class Job(models.Model):
     name = models.CharField(max_length=200, verbose_name="標記工作名稱")
     description = models.TextField(verbose_name="定義與說明")
-    create_date = models.DateTimeField(verbose_name="建立時間")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="建立時間")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="最後更改")
 
     def __str__(self):
         return self.name
 
+    def show_labels(self):
+        return [f'"{label.name}"' for label in self.label_set.all()]
+
+    show_labels.admin_order_field = 'updated_at'
+    show_labels.boolean = False
+    show_labels.short_description = '存在的標籤'
+
+    def show_document_count(self):
+        return len(self.document_set.all())
+
+    show_document_count.boolean = False
+    show_document_count.short_description = '文章數量'
+
+
+class Label(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, verbose_name="所屬任務")
+    # documents = models.ManyToManyField(Document)
+    name = models.CharField(max_length=100, verbose_name="標籤名稱")
+    description = models.TextField(verbose_name="標籤定義")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="建立時間")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="最後更改")
+
+    def __str__(self):
+        return self.name
+
+    def show_document_count(self):
+        return len(self.document_set.all())
+
+    show_document_count.boolean = False
+    show_document_count.short_description = '文章數量'
+
 
 class Document(models.Model):
-    labeling_job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, verbose_name="所屬任務")
     title = models.CharField(max_length=512, verbose_name="標題")
     author = models.CharField(max_length=200, verbose_name="作者")
     s_area_id = models.CharField(max_length=100, verbose_name="頻道id")
     content = models.TextField(verbose_name="內文")
     post_time = models.DateTimeField(verbose_name="發布時間")
+    labels = models.ManyToManyField(Label, verbose_name="被標記標籤")
 
     def __str__(self):
         return self.title
 
+    def show_labels(self):
+        return [f'"{label.name}"' for label in self.labels.all()]
 
-class Label(models.Model):
-    labeling_job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    documents = models.ManyToManyField(Document)
-    name = models.CharField(max_length=100, verbose_name="標籤名稱")
-    description = models.TextField(verbose_name="標籤定義")
-
-    def __str__(self):
-        return self.name
+    show_labels.admin_order_field = 'updated_at'
+    show_labels.boolean = False
+    show_labels.short_description = '被標記標籤'
