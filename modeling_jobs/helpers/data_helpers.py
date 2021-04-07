4931b9e7b6d0acf41b3d1829de18e0288b0199c8
Tai-Siang Bo [柏泰祥] <taisiangbo@eland.com.tw>
2021-04-07 13:31:54 +0800

    Stash pop from 3-modeling-jobs

---
+++ modeling_jobs/helpers/data_helpers.py
@@ -0,0 +1,117 @@
+import cchardet
+import csv
+import codecs
+from labeling_jobs.models import LabelingJob, Document, Label
+from modeling_jobs.models import ModelingJob,Report
+import hashlib
+from datetime import datetime
+import json
+
+class DataHelper:
+    def __init__(self):
+        pass
+
+    def insert_csv_to_db(self, file, job_id):
+        encoding = cchardet.detect(file.read())['encoding']
+        file.seek(0)
+        csv_file = csv.DictReader(codecs.iterdecode(file, encoding))
+        title = csv_file.fieldnames
+        required_fields = ['title', 'author', 's_area_id', 'content', 'label']
+        result = True
+        if set(title) != set(required_fields):
+            return False
+
+        now = datetime.now()
+        dt_string = now.strftime("%Y/%m/%d %H:%M:%S")
+        hash_str = dt_string + file.name
+
+
+        doc_bulk_list = []
+        m = hashlib.md5()
+        m.update(hash_str.encode("utf-8"))
+        hash_num = m.hexdigest()
+
+        job: LabelingJob = LabelingJob.objects.get(pk=job_id)
+        labels = []
+
+        for index,row in enumerate(csv_file):
+            doc = Document(title=row.get("title", ""),
+                           author=row.get("author", ""),
+                           s_area_id=row.get("s_area_id", ""),
+                           content=row.get("content", ""),
+                           post_time=row.get("post_time", None),
+                           labeling_job_id=job.id,
+                           hash_num = hash_num)
+            doc_bulk_list.append(doc)
+            labels.append(row.get("label", None))
+
+        job.document_set.bulk_create(doc_bulk_list)
+
+        documents = job.document_set.filter(hash_num = hash_num)
+        for index,doc in enumerate(documents):
+            label = labels[index]
+            if label:
+                if len(ls := job.label_set.filter(name=label)) > 0:
+                    l = ls.first()
+                    doc.labels.add(l)
+                else:
+                    doc.labels.create(name=label, labeling_job_id=job.id)
+        return result
+
+    def get_training_data(self,labeling_job_id):
+        documents = Document.objects.filter(labeling_job_id = labeling_job_id)
+        content = []
+        labels = []
+        for doc in documents:
+            content.append(doc.content)
+            labels.append([label.name for label in doc.labels.all()])
+        return content,labels
+
+    def get_test_data(self,file):
+        encoding = cchardet.detect(file.read())['encoding']
+        file.seek(0)
+        csv_file = csv.DictReader(codecs.iterdecode(file, encoding))
+        title = csv_file.fieldnames
+        content = []
+        labels = []
+        required_fields = ['title', 'author', 's_area_id', 'content', 'label']
+        if set(title) != set(required_fields):
+            return content,labels
+        else:
+            for item in csv_file:
+                content.append(item.get('content',''))
+                labels.append(item.get('label',''))
+            return content,labels
+
+    def save_report(self,modeling_job_id,report):
+        data = Report.objects.filter(models_ref_id = modeling_job_id)
+        job = ModelingJob.objects.filter(id = modeling_job_id)
+        accuracy = report.get('accuracy', '')
+        if len(data) == 0:
+            r = Report(accuracy = accuracy , report = str(report), models_ref_id = modeling_job_id)
+            r.save()
+        else:
+            data.update(accuracy = accuracy,report = report)
+        job.update(status = True)
+
+    def get_report(self,modeling_job_id):
+        modeling_job_id = int(modeling_job_id)
+        report = Report.objects.get(models_ref_id = modeling_job_id)
+        report = report.report
+        report = report.replace("\'", "\"")
+        report = json.loads(report)
+        for key in report.keys():
+            if key != 'accuracy':
+                for i in report[key]:
+                    report[key][i] = round(report[key][i],3)
+        return report
+
+
+    def get_labels(self,modeling_job_id):
+        job = ModelingJob.objects.get(id = modeling_job_id)
+        labeling_job_id = job.jobRef_id
+        label_set = Label.objects.filter(labeling_job_id = labeling_job_id)
+        labels = []
+        for label in label_set:
+            labels.append(label.name)
+        return labels
--- modeling_jobs/views.py
+++ modeling_jobs/views.py
@@ -1,12 +1,14 @@
 from labeling_jobs.models import LabelingJob, Document
 from .models import ModelingJob, MLModel
-from django.views.generic import ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from core.helpers.data_helpers import DataHelper
-from core.helpers.model_helpers import RuleModel, KeywordModel, ProbModel, RFModel, SvmModel, XgboostModel
-
+from modeling_jobs.helpers.data_helpers import DataHelper
+from modeling_jobs.helpers.model_helpers import RuleModel, KeywordModel, ProbModel, RFModel, SvmModel, XgboostModel
+from django.template.response import TemplateResponse
+from django.shortcuts import render,redirect
+from collections import namedtuple
 
 class IndexView(LoginRequiredMixin, ListView):
     model = ModelingJob
@@ -20,12 +22,12 @@ class IndexView(LoginRequiredMixin, ListView):
         return context
 
 
-class ModelInfoView(LoginRequiredMixin, DetailView):
+class modelInfoView(LoginRequiredMixin, DetailView):
     model = LabelingJob
     template_name = "modeling_jobs/model_info.html"
 
 
-class DocDeleteView(LoginRequiredMixin, DetailView):
+class docDeleteView(LoginRequiredMixin, DetailView):
     model = LabelingJob
 
     def post(self, request):
@@ -33,7 +35,7 @@ class DocDeleteView(LoginRequiredMixin, DetailView):
 
 
 @csrf_exempt
-def doc_delete(request, model_id):
+def docDelete(request, model_id):
     doc_id = request.POST["doc_id"]
     doc = Document.objects.filter(id=doc_id)
     doc.delete()
@@ -41,7 +43,7 @@ def doc_delete(request, model_id):
 
 
 @csrf_exempt
-def doc_update(request, model_id):
+def docUpdate(request, model_id):
     doc_id = request.POST["id"]
     doc = Document.objects.get(id=doc_id)
     doc.title = request.POST["title"]
@@ -53,7 +55,7 @@ def doc_update(request, model_id):
 
 
 @csrf_exempt
-def create_task(request):
+def createTask(request):
     modelingJob = ModelingJob()
     modelingJob.name = request.POST['model_name']
     modelingJob.description = request.POST['description']
@@ -65,7 +67,7 @@ def create_task(request):
 
 
 @csrf_exempt
-def update_task(request):
+def updateTask(request):
     m = ModelingJob.objects.get(id=request.POST['id'])
     m.name = request.POST['model_name']
     m.model_id = request.POST['model_type']
@@ -77,7 +79,7 @@ def update_task(request):
 
 
 @csrf_exempt
-def delete_task(request):
+def deleteTask(request):
     m = ModelingJob.objects.get(id=request.POST['id'])
     m.delete()
     return HttpResponse("Successfully delete!")
@@ -96,9 +98,10 @@ def insert_csv(request):
 def training_model(request):
     jobRef_id = request.POST['jobRef_id']
     model_type = request.POST['model']
+    is_multi_label = request.POST['is_multi_label']
+    modeling_job_id = request.POST['modeling_job_id']
     dataHelper = DataHelper()
     content, labels = dataHelper.get_training_data(jobRef_id)
-
     if model_type == 'RULE_MODEL':
         ruleModel = RuleModel()
     elif model_type == 'KEYWORD_MODEL':
@@ -109,7 +112,67 @@ def training_model(request):
         rfModel = RFModel()
     elif model_type == 'SVM_MODEL':
         svmModel = SvmModel()
+        if is_multi_label == "False":
+            svmModel.fit(content, labels ,modeling_job_id)
+        else:
+            svmModel.multi_fit(content, labels,modeling_job_id)
     elif model_type == 'XGBOOST_MODEL':
         xgboostModel = XgboostModel()
+        if is_multi_label == "False":
+            xgboostModel.fit(content,labels,modeling_job_id)
+        else:
+            xgboostModel.multi_fit(content,labels,modeling_job_id)
+    return HttpResponse("Done")
 
-    return HttpResponse("123")
+@csrf_exempt
+def testing_model(request):
+    file = request.FILES['file']
+    modeling_job_id = request.POST['job_id']
+    model_type = request.POST['model_type']
+    is_multi_label = request.POST['is_multi_label']
+    dataHelper = DataHelper()
+    content,labels = dataHelper.get_test_data(file)
+    if content == [] and labels == []:
+        return HttpResponse("False")
+    else:
+        if model_type == 'RULE_MODEL':
+            ruleModel = RuleModel()
+        elif model_type == 'KEYWORD_MODEL':
+            keywordModel = KeywordModel()
+        elif model_type == 'PROB_MODEL':
+            probModel = ProbModel()
+        elif model_type == 'RF_MODEL':
+            rfModel = RFModel()
+        elif model_type == 'SVM_MODEL':
+            svmModel = SvmModel()
+            if is_multi_label == 'False':
+                svmModel.predict(content,labels,modeling_job_id)
+            else:
+                svmModel.predict_multi_label(content,labels,modeling_job_id)
+        elif model_type == 'XGBOOST_MODEL':
+            xgboostModel = XgboostModel()
+            if is_multi_label == 'False':
+                result = xgboostModel.predict(content,labels,modeling_job_id)
+            else:
+                xgboostModel.predict_multi_label(content,labels,modeling_job_id)
+        return HttpResponse('Done')
+
+@csrf_exempt
+def result_page(request,modeling_job_id):
+    dataHelper = DataHelper()
+    reports : dict = dataHelper.get_report(modeling_job_id)
+    accuracy = reports.pop('accuracy')
+    macro_avg = reports.pop('macro avg')
+    macro_avg['f1_score'] = macro_avg['f1-score']
+    weighted_avg = reports.pop('weighted avg')
+    weighted_avg['f1_score'] = weighted_avg['f1-score']
+    label_info = namedtuple('label_info', ['label','precision', 'recall', 'f1_score','support'])
+    labels = []
+    for key in reports.keys():
+        label = reports.get(key)
+        s = label_info(key,label.get('precision'),label.get('recall'),label.get('f1-score'),label.get('support'))
+        labels.append(s)
+    return render(request,'modeling_jobs/result.html',{"accuracy" : accuracy,
+                                                       "macro_avg" : macro_avg,
+                                                       "weighted_avg" : weighted_avg,
+                                                       "labels" : labels})
