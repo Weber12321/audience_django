c1144f685e5b1d67ae86646338ef7eab3ecda1e7
weifan <p988744@gmail.com>
2021-11-09 16:04:12 +0800

    change labeling jobs api ordering desc

--- labeling_jobs/views.py
+++ labeling_jobs/views.py
@@ -434,7 +434,7 @@ class LabelingJobsSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = LabelingJob.objects.all().order_by("created_at")
+    queryset = LabelingJob.objects.all().order_by("-created_at")
     serializer_class = LabelingJobSerializer
     # permission_classes = [permissions.IsAuthenticated]
 
